f05a94d582ad6ff2c4c3b17b5bb7f85f2568f38b
Vadim <vadim-perog@mail.ru>
2022-08-06 15:35:26 +0300

    2

---
+++ lesson2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 3 числа");
+int x= Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
+if (x >= y && x >= z)
+{
+  Console.WriteLine($"max , {x} " );
+}
+else if (y >= x && y >=z )
+{
+  Console.WriteLine( $"max {y}"  );
+}
+else if (z >= x && z >= y)
+{
+  Console.WriteLine( $"max {z}" );
+}
+
