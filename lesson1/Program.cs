998d360922835b847f1d03ff76fc9bfa0b64ec99
Vadim <vadim-perog@mail.ru>
2022-08-06 13:54:48 +0300

    1

---
+++ lesson1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите 2 числа для поиска максимума");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num > num2) {
+  Console.WriteLine(num);
+}
+else {
+  Console.WriteLine(num2);
+}
