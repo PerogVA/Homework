61d9708d05763c79ef455d07647f2e0571a1523c
Vadim <vadim-perog@mail.ru>
2022-08-11 18:39:46 +0300

    15

---
+++ task15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 1 && num <= 5)
+{
+  Console.WriteLine("нет");
+}
+else if (num > 5 && num <= 7)
+{
+  Console.WriteLine("да");
+}
+else
+{
+  Console.WriteLine("Число не входит в диапазон");
+}
