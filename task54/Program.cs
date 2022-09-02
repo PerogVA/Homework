ba172c85f392ff3a9394ecbff6fb795e1bf0bf6a
Vadim <vadim-perog@mail.ru>
2022-09-02 20:09:12 +0300

    54

---
+++ task54/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+
+  int[,] matrix = new int[row, col];
+  Random rnd = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++) // строки
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
+    {
+      matrix[i, j] = rnd.Next(min, max + 1);
+
+    }
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("[");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write(j < matrix.GetLength(1) - 1 ?
+      $"{matrix[i, j],4}," : $"{matrix[i, j],4} ");
+    }
+    Console.WriteLine("]");
+  }
+}
+
+void ArrangeMatrix(int[,] matrix)
+{
+  for (int k = 0; k < matrix.GetLength(0); k++)
+  {
+
+
+    for (int i = 0; i < matrix.GetLength(1)-1; i++)
+    {
+      int index = i;
+      int max = matrix[k,i];
+      for (int j = i+1; j < matrix.GetLength(1); j++)
+      {
+        if (max < matrix[k,j])
+        {
+          max = matrix[k,j];
+          index = j;
+        }
+      }
+      int temp = matrix[k,index];
+      matrix[k,index] = matrix[k,i];
+      matrix[k,i] = temp;
+    }
+  }
+}
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+Console.WriteLine();
+ArrangeMatrix(array2D);
+PrintMatrix(array2D);
+
